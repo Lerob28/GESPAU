b7765b6278f2d14e5c6efc9f21c0f1ba051d02e2
Lerob28 <njeunkweborel@gmail.com>
2023-02-13 10:24:01 +0100

    V1 menu et ajout page Liste drivers

---
+++ assets/js/copyrightFooter.js
@@ -0,0 +1,5 @@
+var footerCopyright = document.getElementById('footer_copyright');
+
+var currentYear = new Date().getFullYear();
+
+footerCopyright.innerText = `Copyright © ${currentYear} Borel Njeunkwe`;
